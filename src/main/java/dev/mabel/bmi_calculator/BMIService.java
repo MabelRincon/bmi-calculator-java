a7d8aac9b16e0f9021d8c812a49352b5f71c3e26
MabelRincon <mabel.rincon@outlook.com>
2025-02-10 21:00:30 +0100

    testCalculateBMI passed

--- src/main/java/dev/mabel/bmi_calculator/BMIService.java
+++ src/main/java/dev/mabel/bmi_calculator/BMIService.java
@@ -1,5 +1,8 @@
 package dev.mabel.bmi_calculator;
 
 public class BMIService {
+    public double calculateBMI(double weight, double height) {
+        return weight / Math.pow(height, 2);
+    }
 
 }
