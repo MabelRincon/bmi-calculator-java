c8031445df71571f2d6e7f79c50d0291b1f0ba6a
MabelRincon <mabel.rincon@outlook.com>
2025-02-10 21:48:05 +0100

    testClassifyBMI passed

--- src/main/java/dev/mabel/bmi_calculator/BMIService.java
+++ src/main/java/dev/mabel/bmi_calculator/BMIService.java
@@ -1,21 +1,30 @@
 package dev.mabel.bmi_calculator;
 
 public class BMIService {
-    public double calculateBMI(double weight, double height) {
+    public double calculatorBMI(double weight, double height) {
 
         if (height <= 0 && weight <= 0) {
             throw new IllegalArgumentException("Weight and Height must be positive");
         }
         if (height <= 0) {
             throw new IllegalArgumentException("Height must be positive");
-        /* }
-        if (weight <= 0) {
-            throw new IllegalArgumentException("Weight must be positive"); */
         }
         if (weight < 0 ) {
             throw new IllegalArgumentException("Weight cannot be negative");
         }
         return weight / Math.pow(height, 2);
     }
+
+    public String classifyBMI(double bmi) {
+        if (bmi < 16) return "Severe thinness";
+        else if (bmi < 17) return "Moderate thinness";
+        else if (bmi < 18.5) return "Mild thinness";
+        else if (bmi < 25) return "Normal weight";
+        else if (bmi < 30) return "Overweight";
+        else if (bmi < 35) return "Mild obesity";
+        else if (bmi < 40) return "Moderate obesity";
+        else return "Morbid obesity";
+    }
+
 
 }
--- src/test/java/dev/mabel/bmi_calculator/BMIServiceTest.java
+++ src/test/java/dev/mabel/bmi_calculator/BMIServiceTest.java
@@ -10,57 +10,57 @@ public class BMIServiceTest {
 
     @Test
     @DisplayName("Should calculate BMI")
-    void testCalculateBMI() {
+    void testCalculatorBMI() {
         BMIService bmiService = new BMIService();
-        double bmi = bmiService.calculateBMI(70, 1.75);
+        double bmi = bmiService.calculatorBMI(70, 1.75);
         assertThat(bmi, closeTo(22.86, 0.01));
     }
 
     @Test
     @DisplayName("Should throw exception when height is zero")
-    void testCalculateBMI_ZeroHeight_ShouldThrowException() {
+    void testCalculatorBMI_ZeroHeight_ShouldThrowException() {
         BMIService bmiService = new BMIService();
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            bmiService.calculateBMI(70, 0);
-        bmiService.calculateBMI(70, 0);
+            bmiService.calculatorBMI(70, 0);
+        bmiService.calculatorBMI(70, 0);
         });
         assertThat(exception.getMessage(), containsString("Height must be positive"));
     }
 
     @Test
     @DisplayName("Should return zero when weight is zero")
-    void testCalculateBMI_ZeroWeight_ShouldReturnZero() {
+    void testCalculatorBMI_ZeroWeight_ShouldReturnZero() {
         BMIService bmiService = new BMIService();
-        double bmi = bmiService.calculateBMI(0, 1.75);
+        double bmi = bmiService.calculatorBMI(0, 1.75);
         assertThat(bmi, is(0.0));
     }
 
     @Test
     @DisplayName("Should throw exception when height is negative")
-    void testCalculateBMI_NegativeHeight_ShouldThrowException() {
+    void testCalculatorBMI_NegativeHeight_ShouldThrowException() {
         BMIService bmiService = new BMIService();
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            bmiService.calculateBMI(70, -1.75);
+            bmiService.calculatorBMI(70, -1.75);
         });
         assertThat(exception.getMessage(), containsString("Height must be positive"));
     }
 
     @Test
     @DisplayName("Should throw exception when weight is negative")
-    void testCalculateBMI_NegativeWeight_ShouldThrowException() {
+    void testCalculatorBMI_NegativeWeight_ShouldThrowException() {
         BMIService bmiService = new BMIService();
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            bmiService.calculateBMI(-70, 1.75);
+            bmiService.calculatorBMI(-70, 1.75);
         });
         assertThat(exception.getMessage(), containsString("Weight cannot be negative"));
     }
 
     @Test
     @DisplayName("Should throw exception when weight and height are negative")
-    void testCalculateBMI_NegativeWeightAndHeight_ShouldThrowException() {
+    void testCalculatorBMI_NegativeWeightAndHeight_ShouldThrowException() {
         BMIService bmiService = new BMIService();
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            bmiService.calculateBMI(-70, -1.75);
+            bmiService.calculatorBMI(-70, -1.75);
         });
         assertThat(exception.getMessage(), containsString("Weight and Height must be positive"));
     }
