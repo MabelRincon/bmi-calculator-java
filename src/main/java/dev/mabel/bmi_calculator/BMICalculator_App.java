032bc7fa4a0b8583417e3f29628ff2133abf2cf9
MabelRincon <mabel.rincon@outlook.com>
2025-02-10 22:13:21 +0100

    testBMICalculatorApp Happy path passed

--- src/main/java/dev/mabel/bmi_calculator/BMICalculator_App.java
+++ src/main/java/dev/mabel/bmi_calculator/BMICalculator_App.java
@@ -1,9 +1,25 @@
 package dev.mabel.bmi_calculator;
+
+import java.util.Scanner;
 public final class BMICalculator_App {
     private BMICalculator_App() {}
 
     public static void main(String[] args) {
-        System.out.println("My Java Template");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your weight (kg): ");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Enter your height (m): ");
+        double height = scanner.nextDouble();
+
+        BMIService bmiService = new BMIService();
+        double bmi = bmiService.calculatorBMI(weight, height);
+        String category = bmiService.classifyBMI(bmi);
+
+        System.out.println("Your BMI is: " + bmi);
+        System.out.println("Category: " + category);
+
+        scanner.close();
     }
 }
